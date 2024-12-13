23e8591ee610508f6275dd0d68bc36355bca72b4
Serg Kuchukov Office <kuchukov.sergey@gmail.ru>
2024-12-13 12:01:26 +0300

    evn

--- bd_vacansey/main.py
+++ bd_vacansey/main.py
@@ -3,7 +3,7 @@ from src.list_data import ListData
 #from config import LIST_COMPANY, WHITE, YELLOW, RESET_COLOR
 from src.indastries import Industries
 from src.db_connect import DBConnect
-
+from src.color import color
 import logging
 
 logger_info = logging.getLogger(__name__)
@@ -12,6 +12,13 @@ console_handler = logging.StreamHandler()
 logger_info.addHandler(console_handler)
 logger_info.setLevel(logging.INFO)
 
+logger_er = logging.getLogger(__name__)
+# Создаем хендлер для вывода в консоль
+console_handler_er = logging.StreamHandler()
+console_formatter = logging.Formatter('Error %(filename)s строка %(lineno)d')
+console_handler_er.setFormatter(console_formatter)
+logger_er.addHandler(console_handler_er)
+logger_er.setLevel(logging.INFO)
 
 def main():
     """x = GetAPI()
@@ -31,8 +38,34 @@ def main():
     if er > 0:
         logger_info.warning(YELLOW + "Есть не загруженные компании"+ RESET_COLOR)
      # print(ListData.company(list_company_json))"""
-
     q = DBConnect()
+
+    # Заполнение БД industries
+    industries = Industries()
+    print(industries.load())
+    conn = DBConnect.connect()
+    cur = conn.cursor()
+    cur.execute('delete FROM industries;')
+    conn.commit()
+    sql = ''
+    for key , value in Industries.dict_industries.items():
+        if i == 0: sql = "INSERT INTO industries VALUES "
+        sql = sql + f"({key}, '{value}')"
+        i += 1
+        if i == 10:
+            sql = sql + ", "
+        else:
+            i = 0
+            sql = sql + ";"
+            cur.execute(sql)
+    conn.commit()
+
+
+    x = q.select('SELECT * FROM industries')
+    if q.status == 'Error':
+        logger_er.error(color('red', "Ошибка SQL запроса "))
+
+    print(x)
 
 
     # с = x.company(780654)
--- src/db_connect.py
+++ src/db_connect.py
@@ -1,7 +1,7 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
-from typing import Any
+from typing import  Any
 import re
 
 # Загрузка переменных из .env-файла
@@ -13,7 +13,6 @@ SQL_PASS = os.getenv("SQL_PASS")
 SQL_DATABASE_ADMIN = re.sub("[^A-Za-z0-9]", "", os.getenv("SQL_DATABASE_ADMIN"))
 
 import logging
-#from config import LIST_COMPANY, WHITE, YELLOW, RESET_COLOR
 from src.color import color
 
 logger_info = logging.getLogger(__name__)
@@ -22,34 +21,14 @@ console_handler = logging.StreamHandler()
 logger_info.addHandler(console_handler)
 logger_info.setLevel(logging.INFO)
 
+
+
+
 class DBConnect:
     """ Соединение с Базой Данных. Проверка на наличие Бд и таблиц в ней"""
 
-    def __init__(self) -> None:
-        self.status = 'Ok'
-        try:
-            conn = psycopg2.connect(
-                host=SQL_HOST,
-                database=SQL_DATABASE_ADMIN,
-                user=SQL_USER,
-                password=SQL_PASS
-            )
-        except Exception as e:
-            self.status = f'Error {e}'
-        conn.autocommit = True
-        cur = conn.cursor()
-        try:
-            cur.execute("SELECT datname  FROM pg_database WHERE datname = %s", (SQL_DATABASE,))
-        except Exception as e:
-            self.status = f'Error {e}'
-        if not cur.fetchone():
-            # если нет БД, то создадим
-            cur.execute(f"CREATE DATABASE {SQL_DATABASE} ENCODING 'UTF8' "  )
-            logger_info.info(color('white', "Создали БД") )
-            #logger_info.info(WHITE + "Создали БД" + SQL_DATABASE + RESET_COLOR)
-
-        cur.close()
-        conn.close()
+    @staticmethod
+    def connect() -> Any:
         try:
             conn = psycopg2.connect(
                 host=SQL_HOST,
@@ -58,32 +37,123 @@ class DBConnect:
                 password=SQL_PASS
             )
         except Exception as e:
+            logger_info.error(color('red', f"Error: Ошибка соединения с БД {e}" ))
+            quit()
+        return conn
+
+    def __init__(self) -> None:
+        self.status = 'Ok'
+        self.__cur = None
+        try:
+            conn = psycopg2.connect(
+                host=SQL_HOST,
+                database=SQL_DATABASE_ADMIN,
+                user=SQL_USER,
+                password=SQL_PASS
+            )
+        except Exception as e:
+            logger_info.error(color('red', f"Error: Ошибка соединения с БД {e}" ))
+            quit()
+        conn.autocommit = True
+        cur = conn.cursor()
+        try:
+            cur.execute("SELECT datname  FROM pg_database WHERE datname = %s", (SQL_DATABASE,))
+        except Exception as e:
             self.status = f'Error {e}'
+        if not cur.fetchone():
+            # если нет БД, то создадим
+            cur.execute(f"CREATE DATABASE {SQL_DATABASE} ENCODING 'UTF8' ")
+            logger_info.info(color('white', "Создали БД"))
+            # logger_info.info(WHITE + "Создали БД" + SQL_DATABASE + RESET_COLOR)
+
+        cur.close()
+        conn.close()
+
+        # .connect с БД
+        conn = DBConnect.connect()
+        # Загружаем названия таблиц из БД
+        conn.autocommit = True
         cur = conn.cursor()
         try:
             cur.execute("SELECT tablename FROM pg_tables WHERE schemaname = 'public'")
         except Exception as e:
             self.status = f'Error {e}'
         rows = cur.fetchall()
-        print(rows)
-
+        # Проверяем есть ли таблицы в БД, если нет - создаем
         if not ('company',) in rows:
             logger_info.info(color('white', "создается таблица company"))
-            #logger_info.info(WHITE + "создается таблица company" + RESET_COLOR)
+            try:
+                cur.execute("CREATE TABLE company ( company_id int PRIMARY KEY);")
+            except Exception as e:
+                self.status = f'Error {e}'
+                logger_info.error(color('red', "Error CREATE TABLE company"))
+            else:
+                self.status = 'Ok'
+                logger_info.info(color('white', "Ok"))
         if not ('vacancies',) in rows:
             logger_info.info(color('white', "создается таблица vacancies"))
-            #logger_info.info(WHITE + "создается таблица vacancies" + RESET_COLOR)
+            try:
+                cur.execute("CREATE TABLE vacancies ( vacancies_id int PRIMARY KEY);")
+            except Exception as e:
+                self.status = f'Error {e}'
+                logger_info.error(color('red', "Error CREATE TABLE vacancies"))
+            else:
+                self.status = 'Ok'
+                logger_info.info(color('white', "Ok"))
+
         if not ('industries',) in rows:
             logger_info.info(color('white', "создается таблица industries"))
-            #logger_info.info(WHITE + "создается таблица industries" + RESET_COLOR)
+            try:
+                cur.execute("""CREATE TABLE industries (
+                industries_id float PRIMARY KEY, 
+                industries_name varchar(255) NOT NULL
+                );""")
 
+            except Exception as e:
+                self.status = f'Error {e}'
+                logger_info.error(color('red', "Error CREATE TABLE industries"))
+            else:
+                self.status = 'Ok'
+                logger_info.info(color('white', "Ok"))
 
-        self.__connect = conn
+        cur.close()
+        conn.close()
         self.status = 'Ok'
 
 
-    @property
-    def connect(self):
-        return self.__connect
+    '''@property
+    def connect(self) -> object:
+        return self.__connect'''
 
+    def select(self, sql_txt: str) -> list:
+        """ Соединяется с БД и
+        отправляет запрос SQL
+        возвращет список строк или пустой список"""
+        lits_word_sql = sql_txt.split(' ')
+        if lits_word_sql[0] != 'SELECT':
+            self.status = 'Error Неверный формат запроса'
+            return []
+        # .connect с БД
+        conn = DBConnect.connect()
+        cur = conn.cursor()
+        try:
+            cur.execute(sql_txt)
+        except Exception:
+            self.status = 'Error'
+            cur.close()
+            conn.close()
+            return []
+        else:
+            rows = cur.fetchall()
+            self.status = 'Ok'
+            cur.close()
+            conn.close()
+            return rows
 
+    def delete(self):
+        "удаляет данные из таблиц"
+        pass
+
+    def reset(self):
+        "удаляет полностью БД"
+        pass
